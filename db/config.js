7757e21cbb800eb007f580ad7396d2974c0216bc
raprmdn <rafiputraramadhan14@gmail.com>
2022-11-17 21:43:43 +0700

    add port

--- db/config.js
+++ db/config.js
@@ -22,13 +22,15 @@ module.exports = {
     "password": DB_PASSWORD,
     "database": DB_NAME,
     "host": DB_HOST,
-    "dialect": DB_DIALECT
+    "dialect": DB_DIALECT,
+    "port": DB_PORT
   },
   "production": {
     "username": DB_USERNAME,
     "password": DB_PASSWORD,
     "database": DB_NAME,
     "host": DB_HOST,
-    "dialect": DB_DIALECT
+    "dialect": DB_DIALECT,
+    "port": DB_PORT
   }
 };
